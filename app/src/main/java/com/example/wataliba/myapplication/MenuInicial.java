a14b75d7ab6461888b50b06f166519f51542c647
Werichinilson Abreu Ataliba <agentwerich@gmail.com>
2017-07-09 18:45:59 -0300

    Tela Menu Inicial prototipo

---
+++ app/src/main/java/com/example/wataliba/myapplication/MenuInicial.java
@@ -0,0 +1,7 @@
+package com.example.wataliba.myapplication;
+
+/**
+ * Created by wataliba on 07/07/2017.
+ */
+public class MenuInicial {
+}
---
+++ app/src/main/java/com/example/wataliba/myapplication/Repository/RepositoryTables.java
@@ -0,0 +1,7 @@
+package com.example.wataliba.myapplication.Repository;
+
+/**
+ * Created by wataliba on 07/07/2017.
+ */
+public class RepositoryTables {
+}
